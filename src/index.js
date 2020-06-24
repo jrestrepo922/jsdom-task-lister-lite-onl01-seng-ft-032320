b8c32b7c9fb36b2fc9ea04cef39775fe4f2de226
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-24 18:16:19 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   
   let submit = document.querySelector('form input[type="submit"]')
-  
+  let task = document.querySelector(" input#new-task-description")
   
   
   submit.addEventListener("click", (event) => {
@@ -10,9 +10,9 @@ document.addEventListener("DOMContentLoaded", () => {
     // finding the element where the tasks are 
     let tasks = document.querySelector("#tasks");
     // creating a list node to add the new task
-    let task = document.createElement("li");
+    let taskElement = document.createElement("li");
     // providing the text that will be display
-    task.innerHTML = submit.value
+    taskElement.innerHTML = task.value
     // attaching the new li node to the ul list 
     tasks.appendChild(task)
     
