f94d7fdd03f8c3ca83915d769011c1019ba1474a
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-24 18:04:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
   submit.addEventListener("click", (event) => {
     preventDefault();
     let tasks = document.querySelector("#tasks")
-    
+  
   });
   
 });
