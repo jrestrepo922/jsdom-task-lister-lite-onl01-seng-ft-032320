1941d194380460a7be6c5823edef76ca813cdaa1
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-24 17:44:09 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let form = document.querySelector(" input#new-task-description")
+  let textInput = document.querySelector(" input#new-task-description")
   
   
   
