db79bcac320704840eb8f26593baf88047bd76a0
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-24 17:51:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-  let textInput = document.querySelector(" input#new-task-description");
+  
   let submit = document.querySelector('form input[type="submit"]')
   
   
   
   submit.addEventListener("click", (addText) => {
-    
+    let textInput = document.querySelector(" input#new-task-description");
   });
   
 });
