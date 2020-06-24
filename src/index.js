5021089bce7dc6b9eda1e90db818512493d8f51a
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-24 18:11:32 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
   
   
   submit.addEventListener("click", (event) => {
-    preventDefault();
+    preventDefault(event);
     // finding the element where the tasks are 
     let tasks = document.querySelector("#tasks");
     // creating a list node to add the new task
