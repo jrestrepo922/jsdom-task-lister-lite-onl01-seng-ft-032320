23566e74480bd74a3a062ae3b8ee53ec23cfe999
JUAN M RESTREPO <tinto922@gmail.com>
2020-06-24 18:09:31 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ document.addEventListener("DOMContentLoaded", () => {
   
   submit.addEventListener("click", (event) => {
     preventDefault();
-    let tasks = document.querySelector("#tasks")
-  
+    // finding the element where the tasks are 
+    let tasks = document.querySelector("#tasks");
+    // creating a list node to add the new task
+    let task = document.createElement("li");
+    // providing the text that will be display
+    task.innerHTML = submit.value
+    
   });
   
 });
